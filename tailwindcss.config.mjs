1825eb2d180b49506fea2780bc023ab105dcacc4
KingToochi <tu@byupathway.edu>
2025-09-02 20:42:37 +0100

    Initial portfolio commit

---
+++ tailwindcss.config.mjs
@@ -0,0 +1,13 @@
+export default  {
+     content: [
+       "./src/**/*.{js,jsx,ts,tsx}",
+     ],
+     theme: {
+       extend: {
+        fontFamily: {
+        abril: ['"Abril Fatface"', 'cursive'], // or serif, depending on your preference
+      },
+       },
+     },
+     plugins: [],
+   }
